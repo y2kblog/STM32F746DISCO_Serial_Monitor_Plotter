cbe161e6e22d864c697f67fde4b01923d7b3fee5
y2kblog <kambara.y2kb@gmail.com>
2017-04-15 23:57:13 +0900

    Add communication test code
    Add communication test code using NUCLEO-F411RE (mbed). The images were
    obtained by using this test code.

---
+++ CommunicationTestCode_mbed_NUCLEO-F411RE/main.cpp
@@ -0,0 +1,29 @@
+#include "mbed.h"
+
+//------------------------------------
+// Hyperterminal configuration
+// 115200 bauds, 8-bit data, no parity
+//------------------------------------
+
+Serial pc(SERIAL_TX, SERIAL_RX);
+
+int main()
+{
+    float time = 0.0f;
+    float sinval, cosval;
+    pc.baud(115200);
+    
+    while(1) {
+        wait(0.01);
+        sinval = sinf(6.283f * time);
+        cosval = cosf(6.283f * time);
+        
+        pc.printf("%d,%d,%d,%d,%d,%d,%d,%d\n",
+         (int16_t)(100.0f*sinval), (int16_t)(100.0f*cosval),
+         (int16_t)(70.0f*sinval),  (int16_t)(70.0f*cosval),
+         (int16_t)(50.0f*sinval),  (int16_t)(50.0f*cosval),
+         (int16_t)(30.0f*sinval),  (int16_t)(30.0f*cosval)
+         );
+        time += 0.01f;
+    }
+}
